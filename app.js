135ca0fe5d92bc2a4762ccfbd4d5ab1b20997e9b
Mohamed Rabea Mahran <med886418@gmail.com>
2025-08-04 11:20:59 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 function toggleMenu() {
   const menu = document.getElementById("sideMenu");
-  menu.style.display = menu.style.display === "flex" ? "none" : "flex";
+  menu.classList.toggle("show");
 }
+
+// إغلاق القائمة عند الضغط خارجها
+document.addEventListener("click", function (e) {
+  const menu = document.getElementById("sideMenu");
+  const toggleBtn = document.querySelector(".menu-toggle");
+
+  if (!menu.contains(e.target) && !toggleBtn.contains(e.target)) {
+    menu.classList.remove("show");
+  }
+});
